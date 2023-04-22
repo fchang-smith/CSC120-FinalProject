7d42bf68b14ba319e378c535c2856859e4c2fe76
Feiran Chang <fchang@smith.edu>
2023-04-21 23:04:57 -0400

    damn

--- HuffmanCode.java
+++ HuffmanCode.java
@@ -3,6 +3,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Hashtable;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class HuffmanCode {
   ArrayList<String> wordList;
@@ -33,6 +35,8 @@ public class HuffmanCode {
         e.printStackTrace();}
     }
 
+
+
     public void buildTree() {
       if (this.wordList == null) {
         throw new RuntimeException("You need to load a text file using loadFile() first!");
@@ -56,11 +60,118 @@ public class HuffmanCode {
       }
     }
 
+    public void printTree () {
+      Node rootNode = this.tree.findRootNode();
+      ArrayList<Node> nodeList = new ArrayList<Node>();
+      nodeList.add(rootNode);
+      System.out.print(rootNode.getValue() + " ");
+      System.out.println();
+      boolean loop = true;
+      while (loop) {
+        nodeList = this.tree.printTree(nodeList);
+        if (nodeList == null) {
+          loop = false;
+        }
+      }
+    }
+
+    public void printTreeWord () {
+      Node rootNode = this.tree.findRootNode();
+      ArrayList<Node> nodeList = new ArrayList<Node>();
+      nodeList.add(rootNode);
+      System.out.print(rootNode.getValue() + " ");
+      System.out.println();
+      boolean loop = true;
+      while (loop) {
+        nodeList = this.tree.printTreeWord(nodeList);
+        if (nodeList == null) {
+          loop = false;
+        }
+      }
+    }
+
+    public String generateCode() {
+      String code = "";
+      for (String word : this.wordList) {
+        code += this.tree.generateCode(word);
+      }
+      System.out.println(code);
+      return code;
+    }
+
+    public String generateCode(String word) {
+      String code = "";
+      code = this.tree.generateCode(word);
+      System.out.println(code);
+      return code;
+    }
+
+    
+
+    public String deCode(String code) {
+      char[] path = code.toCharArray();
+      int idx;
+      Node upNode = this.tree.findRootNode();
+      Node downNode = null;
+      for (int i = 0; i < path.length; i++) {
+        if (upNode.hasChild()){
+          idx = i;
+          char edge = path[i];
+          if (edge == 0) {
+            downNode = upNode.getLChild();
+          } else if (edge == 1) {
+            downNode = upNode.getRChild();
+          }
+          upNode = downNode;
+        } else{
+          break;
+        }
+      }
+      return "";
+    }
+
+    private int convertCode(int idx, char[] path, String filePath) {
+      Node upNode = this.tree.findRootNode();
+      Node downNode = null;
+      try {
+        File newFile = new File(filePath);
+        newFile.createNewFile();
+      } catch (IOException e) {
+        e.getMessage();
+      }
+      for (int i = idx; i < path.length; i++) {
+        idx = i;
+        char edge = path[i];
+        System.out.println("edge: "+edge);
+        if (edge == 0) {
+          downNode = upNode.getLChild();
+        } else {
+          downNode = upNode.getRChild();
+        }
+        if (downNode == null) {
+          try {
+            FileWriter myWriter = new FileWriter(filePath);
+            myWriter.append(upNode.getWord());
+            myWriter.close();
+          } catch (IOException e) {
+            e.getMessage();
+          }
+          break;
+        }
+        upNode = downNode;
+      }
+      return idx;
+    }
+
     public static void main(String[] args) {
         HuffmanCode myCode = new HuffmanCode();
         myCode.loadFile("/Users/fiona/Library/Mobile Documents/com~apple~CloudDocs/smith/Study/2023_Spring/CSC_120/CSC120-FinalProject/TheBoarPrincess.txt");
         myCode.buildTree();
-        
+        myCode.generateCode();
+        myCode.printTreeWord();
+        char [] path = "1100".toCharArray();
+        int num = myCode.convertCode(0, path, "/Users/fiona/Library/Mobile Documents/com~apple~CloudDocs/smith/Study/2023_Spring/CSC_120/CSC120-FinalProject/TestTheBoarPrincess.txt");
+        System.out.println(num);
+        System.out.println("code for out: " + myCode.generateCode("out"));
     }
-
 }
